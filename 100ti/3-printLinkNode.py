c8644ac0cbeee1b38698c249d2e3ead18c16a471
cheriezhang <zhxxuer@163.com>
2017-10-26 19:38:18 +0800

    100ti 3

--- 100ti/3-printLinkNode.py
+++ 100ti/3-printLinkNode.py
@@ -4,19 +4,19 @@
 # 输入一个链表，从尾到头打印链表每个节点的值。
 # 返回从尾部到头部的列表值序列，例如[1,2,3]
 
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-
-listNodeR = ListNode()
-
-def printListFromTailToHead(self, listNode):
+listNodeR = {}
+def printListFromTailToHead(listNode): # 实际上是倒序输出
     list = []
-    while (listNode is not None):
-        list += listNode.val
-        listNode = listNode.next
-    print list
+    head = listNode
+    while head:
+        list.insert(0, head.val)
+        head = head.next
+    return list
 
+print printListFromTailToHead(listNodeR)
 
